f69e6b1a5eeb6d863697fff8d8cccafd7061d033
YB <yb@gmail.com>
2019-01-06 23:07:15 +0100

    first commit

--- adima-back/src/main/java/com/adima/entity/User.java
+++ adima-back/src/main/java/com/adima/entity/User.java
@@ -49,7 +49,7 @@ public class User extends AbstractEntity {
         this.email = email;
     }
 
-    @JsonIgnore
+//    @JsonIgnore
     public String getPassword() {
         return password;
     }
---
+++ adima-front/src/app/chat/chat.component.ts
@@ -0,0 +1,6 @@
+﻿import {Component, OnInit} from '@angular/core';
+
+
+@Component({templateUrl: 'chat.component.html'})
+export class ChatComponent implements OnInit {
+}
